7ae8121e966774ece768b6a930077508629e797e
xiaoliang <xuliang@lettucegames.com>
2021-09-22 20:27:02 +0800

    change ip

--- config.py
+++ config.py
@@ -1,21 +1,15 @@
 base = {
-    'servers':[
-              {'index':0,
-               'name': 'server1', 
-              'host': '127.0.0.1',
-              'port': 6379,
-              'password': '',
-              'databases':16
-              },
-              {'index':1,
-               'name': 'server2', 
-              'host': '127.0.0.1',
-              'port': 6379,
-              'databases':16
-              },
-          ],
-    'seperator' : ':',
-    'maxkeylen' : 100
+    'servers': [
+        {'index': 0,
+         'name': 'serverRedis',
+         'host': 'www.xiaoliang007.asia',
+         'port': 6379,
+         'password': 'Xlqwert109109',
+         'databases': 16
+         }
+    ],
+    'seperator': ':',
+    'maxkeylen': 100
 }
 media_prefix = "pyred_media"
 
